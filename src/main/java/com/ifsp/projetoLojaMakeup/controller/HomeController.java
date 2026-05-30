d0622728d5d90df0862467b3b171706e3c1f976b
Luiz <ll7125875@gmail.com>
2026-05-30 20:13:06 -0300

    Alguns ajustes

--- src/main/java/com/ifsp/projetoLojaMakeup/controller/HomeController.java
+++ src/main/java/com/ifsp/projetoLojaMakeup/controller/HomeController.java
@@ -1,5 +1,23 @@
 package com.ifsp.projetoLojaMakeup.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class HomeController {
     
+    @GetMapping("/")
+    public String abrirHome(){
+        return "home";
+    }
+
+    @GetMapping("/login")
+    public String abrirLogin(){
+        return "login";
+    }
+
+    @GetMapping("/cadastro")
+    public String abrirCadastro(){
+        return "cadastro";
+    }
 }
