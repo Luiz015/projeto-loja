ed045cc6226e1b57985af6843345746f25e3b6a7
Luiz <ll7125875@gmail.com>
2026-05-30 20:55:54 -0300

    muitas alterações-projeto em 60%

--- src/main/java/com/ifsp/projetoLojaMakeup/controller/HomeController.java
+++ src/main/java/com/ifsp/projetoLojaMakeup/controller/HomeController.java
@@ -1,13 +1,23 @@
 package com.ifsp.projetoLojaMakeup.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.ifsp.projetoLojaMakeup.model.Produto;
+import com.ifsp.projetoLojaMakeup.service.ProdutoService;
 
 @Controller
 public class HomeController {
-    
+    @Autowired
+    private ProdutoService produtoService;
     @GetMapping("/")
-    public String abrirHome(){
+    public String abrirHome(Model model){
+        List<Produto> listaProdutos = produtoService.listarTodos();
+        model.addAttribute("lista", listaProdutos);
         return "home";
     }
 
@@ -20,4 +30,15 @@ public class HomeController {
     public String abrirCadastro(){
         return "cadastro";
     }
+/*
+    @GetMapping("/buscar")
+    public String buscarProduto(String titulo, Model model){
+
+        List<Produto> listaProdutos = produtoService.buscarPorTitulo(titulo);
+
+        model.addAttribute("lista", listaProdutos);
+
+        return "produtos";
+    }
+         */
 }
