28a71cfad292cc5012ee7922edc37193a50091ea
Luiz <ll7125875@gmail.com>
2026-05-30 19:28:53 -0300

    Implementa listagem de produtos

--- src/main/java/com/ifsp/projetoLojaMakeup/controller/ProdutoController.java
+++ src/main/java/com/ifsp/projetoLojaMakeup/controller/ProdutoController.java
@@ -1,8 +1,26 @@
 package com.ifsp.projetoLojaMakeup.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.ifsp.projetoLojaMakeup.model.Produto;
+import com.ifsp.projetoLojaMakeup.service.ProdutoService;
 
 @Controller
 public class ProdutoController {
-    
+
+    @Autowired
+    private ProdutoService produtoService;
+
+    @GetMapping("/produtos")
+    public String listarProdutos(Model model){
+        
+        List<Produto> listaProdutos = produtoService.listarTodos();
+        model.addAttribute("lista", listaProdutos);
+        return "produtos";
+    }
 }
